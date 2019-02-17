0786629dd13acdb643d2544f5019b52689f1fd39
MehediHasan25 <ishanmehadi@gmail.com>
2019-02-17 22:20:39 +0600

    lecture 126 finished

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -7,16 +8,34 @@ class LoginForm extends Component {
         errors: {}
     };
 
+    schema={
+        username: Joi.string().required().label('Username'),
+        password: Joi.string().required().label('Password')
+
+    }; 
+
     validate = () => {
-        const errors = {};
+        const options= {abortEarly:false};
+       const {error} = Joi.validate(this.state.account, this.schema, options);
 
-        const { account } = this.state;
-        if (account.username.trim() === '')
-            errors.username = 'Username is required.';
-        if (account.password.trim() === '')
-            errors.password = 'Password is required.';
+        if(!error) return null;
 
-        return Object.keys(errors).length === 0 ? null : errors;
+        const errors={};
+        for (let item of error.details)
+            errors[item.path[0]]= item.message;
+
+            return errors;
+
+        // console.log(result);
+        // const errors = {};
+ 
+        // const { account } = this.state;
+        // if (account.username.trim() === '')
+        //     errors.username = 'Username is required.';
+        // if (account.password.trim() === '')
+        //     errors.password = 'Password is required.';
+
+        // return Object.keys(errors).length === 0 ? null : errors;
     };
 
     handleSubmit = e => {
@@ -31,17 +50,27 @@ class LoginForm extends Component {
     };
 
     validateProperty= ({name, value}) =>{
-        if(name === 'username'){
-            if(value.trim() === '') return 'Username is required.';
-        }
-        //...
-        if(name === 'password'){
-            if(value.trim() === '') return 'Password is required.';
-        } 
+
+        const obj= {[name]: value};
+        const schema = {[name] : this.schema[name]};
+        const {error} = Joi.validate(obj,schema);
+        return error ? error.details[0].message :null; 
+        // if(!error) return null;
+        // return error.details[0].message;
+
+
+        // if(name === 'username'){
+        //     if(value.trim() === '') return 'Username is required.';
+        // }
+        // //...
+        // if(name === 'password'){
+        //     if(value.trim() === '') return 'Password is required.';
+        // } 
 
     };
 
     handleChange = ({ currentTarget: input }) => {
+        
         const errors={...this.state.errors};
         const errorMessage = this.validateProperty(input) ;
         if(errorMessage) errors[input.name] = errorMessage; 
